f925a2e0991d2acefcf893198817418d490b088b
mujiarjo3 <mujiarjo3@gmail.com>
2025-04-14 20:48:08 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# ngobrolngentot
-ngobrolngentot
+[ngobrolngentot](https://ngobrolngentot.pages.dev)
+Fork the [repository](https://github.com/darnomitro) on GitHub.
---
+++ app/layout.tsx
@@ -0,0 +1,82 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
+import { SITENAME } from "@/lib/constants";
+
+const font = Nunito({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+    title: `${SITENAME} - Video Bokep Indo Viral Terbaru`,
+    description: `${SITENAME} Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma korea china tante live paksa ngentot abg cewek pelajar.`,
+    metadataBase: new URL("https://ngobrolngentot.pages.dev"),
+    alternates: {
+        canonical: `/`,
+    },
+    openGraph: {
+        title: `${SITENAME} - Video Bokep Indo Jepang Barat Viral Terbaru`,
+        description: `${SITENAME} Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma korea china tante live paksa ngentot abg cewek pelajar.`,
+        url: `/`,
+        type: `website`,
+    },
+    verification: {
+        google: 'zbnqTEiinpHi4_1AKlztwfNnUYyIv1P0ONgFG8QnMz0',
+        yandex: '6d3b1696c03e167d',
+    },
+};
+
+export const runtime = "edge";
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    const jsonLd = {
+        "@context": "https://schema.org",
+        "@type": "Organization",
+        "name": `${SITENAME}`,
+        "description": `${SITENAME} Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma korea china tante live paksa ngentot abg cewek pelajar.`,
+        "logo": "https://ngobrolngentot.pages.dev/favicon.ico",
+        "url": "https://ngobrolngentot.pages.dev",
+        "founder":{
+            "@type":"Person",
+            "name":"admin",
+            "url":"https://ngobrolngentot.pages.dev"
+            },
+            "foundingDate":"2025-01-25"
+        }
+        const jsonLd2 = {
+            "@context": "https://schema.org",
+        "@type": "WebSite",
+        "name": `${SITENAME}`,
+        "url": "https://ngobrolngentot.pages.dev",
+        "description": `${SITENAME} Video Bokep Indo Jepang Jav Barat Simontok Viral Terbaru Bocil Ngentot Jilbab Smp Mama Sma korea china tante live paksa ngentot abg cewek pelajar.`,
+        "potentialAction": { 
+        "@type": "SearchAction", 
+          "target": "https://ngobrolngentot.pages.dev/?q={search_term}", 
+            "query-input": "required name=search_term"}
+        }
+    return (
+        <html lang="en" suppressHydrationWarning>
+            <body className={font.className}>
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="system"
+                    enableSystem
+                    disableTransitionOnChange
+                >
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd2) }}
+        />
+                    {children}
+                </ThemeProvider>
+            </body>
+        </html>
+    );
+}
